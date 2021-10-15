c00f12bdb82582a00a89d4bb9d48fbb894939ce2
IldarMukhametoff <77933697+IldarMukhametoff@users.noreply.github.com>
2021-10-15 22:57:25 +0500

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -3,5 +3,17 @@ new Swiper('.slider',{
 		nextEl:'.swiper-button-next',
 		prevEl:'.swiper-button-prev'
 	},
+	loop:true,
 	slidesPerView:3,
+	breakpoints:{
+		280:{
+			slidesPerView:1
+		},
+		640:{
+			slidesPerView:2
+		},
+		1024:{
+			slidesPerView:3
+		}
+	}
 });
